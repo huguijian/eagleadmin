4436c53b568bffd4324683ff6df032f63b21bdb7
huguijian <18616725397@163.com>
2024-11-12 11:43:42 +0800

    update

---
+++ src/plugin/eagleadmin/api/User.php
@@ -0,0 +1,31 @@
+<?php
+class User
+{
+    use \plugin\eagleadmin\app\Crud;
+
+
+    /**
+     * 获取所有用户列表
+     * @param \support\Request $request
+     * @return array
+     */
+    public function getUserList(\support\Request  $request)
+    {
+        [$where, $pageSize, $order] = $this->selectInput($request);
+        $model = $this->selectMap($where,$order);
+        if ($this->pageSize == -1) { // 值为-1表示不分页
+            $list = $model->get() ?? [];
+        } else {
+            $pageSize = $this->pageSize > 0 ? $this->pageSize : $pageSize;
+            $paginator = $model->paginate($pageSize);
+            $list = $paginator->items() ?? [];
+            $res['total'] = $paginator->total();
+        }
+        if ($this->callBack && is_callable($this->callBack)) {
+            $list = call_user_func($this->callBack, $list) ?? [];
+        }
+
+        $res['items'] = $list;
+        return $res;
+    }
+}
