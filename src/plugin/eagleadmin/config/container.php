f108efdd17b1325aaad8d4ed787a8c85b218c783
huguijian <18616725397@163.com>
2025-03-05 16:53:45 +0800

    upate

--- src/plugin/eagleadmin/config/container.php
+++ src/plugin/eagleadmin/config/container.php
@@ -1,6 +1,2 @@
 <?php
-$builder = new \DI\ContainerBuilder();
-$builder->addDefinitions(config('dependence', []));
-$builder->useAutowiring(true);
-$builder->useAnnotations(true);
-return $builder->build();
+return new Webman\Container;
