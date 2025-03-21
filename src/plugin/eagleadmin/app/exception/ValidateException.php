acf3cf30fccd7998b1fb75430c5dcafcce073d1c
huguijian <18616725397@163.com>
2025-03-21 17:36:36 +0800

    update

--- src/plugin/eagleadmin/app/exception/ValidateException.php
+++ src/plugin/eagleadmin/app/exception/ValidateException.php
@@ -3,16 +3,15 @@
 namespace plugin\eagleadmin\app\exception;
 
 use Exception;
-
-
+use Webman\Http\Request;
+use Webman\Http\Response;
 class ValidateException extends Exception
 {
 
     public $msg  = "";
+    public $code = 500;
 
     public $data = [];
-
-    public $code = 500;
 
 
     public function __construct($msg, $data=[], $code=-1)
@@ -23,6 +22,20 @@ class ValidateException extends Exception
         $this->data = $data;
         $this->code = $code;
 
+    }
+
+    public function render(Request $request): ?Response
+    {
+        return json(
+            [
+                'code' => $this->getCode() ?? 500,
+                'msg' => $this->getMessage(),
+                'data'=> $this->data,
+            ],
+            JSON_UNESCAPED_UNICODE
+                | JSON_PRETTY_PRINT
+                | JSON_UNESCAPED_SLASHES
+        );
     }
 
 
