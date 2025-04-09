3147064242dc717523902c30e5ede59da787a583
huguijian <18616725397@163.com>
2025-04-09 17:12:15 +0800

    udpate

--- src/plugin/eagleadmin/config/database.php
+++ src/plugin/eagleadmin/config/database.php
@@ -4,11 +4,11 @@ return  [
     'connections' => [
         'mysql' => [
             'driver'      => 'mysql',
-            'host'        => '127.0.0.1',
-            'port'        => '3306',
-            'database'    => 'eagleAdmin',
-            'username'    => 'root',
-            'password'    => '123456',
+            'host'        => getenv('DB_HOST'),
+            'port'        => getenv('DB_PORT'),
+            'database'    => getenv('DB_DATABASE'),
+            'username'    => getenv('DB_USERNAME'),
+            'password'    => getenv('DB_PASSWORD'),
             'charset'     => 'utf8mb4',
             'collation'   => 'utf8mb4_general_ci',
             'prefix'      => '',
