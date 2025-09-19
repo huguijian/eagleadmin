d6787d16347f9c9666ac333e406860f4d5419086
huguijian <18616725397@163.com>
2025-09-19 09:50:25 +0800

    upate

--- src/plugin/eagleadmin/app/model/Base.php
+++ src/plugin/eagleadmin/app/model/Base.php
@@ -3,9 +3,14 @@
 namespace plugin\eagleadmin\app\model;
 
 use support\Model;
-
+use DateTimeInterface;
 
 class Base extends Model
 {
     protected $connection = 'plugin.eagleadmin.mysql';
+
+     protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
